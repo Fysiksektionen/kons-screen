964bcde77ff6befcc3c0e33c8b3062bd96d61de3
Acksell <axelen@kth.se>
2017-11-22 12:36:43 +0100

    Removed flask-sockets in favour of jQuery $.get() calls.

--- app.py
+++ app.py
@@ -2,25 +2,23 @@ import time
 import json
 
 from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
 
 from update_db import DB
 
 app = Flask(__name__)
-socketio = SocketIO(app)
 DB_FILE = "db.json"
 
 @app.route('/')
 def index():
     db = DB(DB_FILE, read_only=True)
-    return render_template('feed.html', db=db)  # jinja2 template
+    return render_template('feed.html', db=db, time=time.strftime("%H:%M:%S"))  # jinja2 template
 
-@socketio.on('update')
-def update_data():
-    # TODO: should only send data that was changed so as to not use unnecessary bandwidth.
+# This route can be removed once we have a remote server implemented and
+# if we decide to not use a python script "middleman" to fetch the data.
+@app.route('/update')
+def update():
     db = DB(DB_FILE, read_only=True)
-    # sending time to prove dynamic content works.
-    emit('server response', {'time': time.strftime("%H:%M:%S"), **db})
+    return json.dumps({'time': time.strftime("%H:%M:%S"), **db})
 
 if __name__ == '__main__':
-    socketio.run(app)
+    app.run()
