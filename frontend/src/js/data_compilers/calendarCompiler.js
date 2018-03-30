46380883d135ef0e6f761f8a7e0b58ee334bf107
Acksell <axelen@kth.se>
2018-03-30 14:27:59 +0200

    Restructured calendarCompiler to be more testable

--- frontend/src/js/data_compilers/calendarCompiler.js
+++ frontend/src/js/data_compilers/calendarCompiler.js
@@ -1,25 +1,42 @@
 // Module containing functions which compile the calendar data fetched.
 
-var compileCalendar = function (calendar){
-    var getDate = (e) => {
-        if (e.start.dateTime)
-            return e.start.dateTime
-        else if (e.start.date)
-            return e.start.date
-        else 
-            return 0
-    }
+var getDate = (event) => {
+    let date
+    if (event.start.dateTime)
+        date = event.start.dateTime
+    else if (event.start.date)
+        date =  event.start.date
+    else 
+        date =  0
+    return new Date(date).toLocaleString()
+}
 
-    var events = calendar
-        .map( (e) => {
-            return {
-                date: (new Date(getDate(e))).toLocaleString(),
-                location: e.location,
-                name: e.summary }
-            })
-    return {calendar: {events}}
+var remapperFactory = function (dateGetter){
+    return event => {
+        return {
+            date: dateGetter(event),
+            location: event.location,
+            name: event.summary 
+        }
+    }
+}
+
+var remapEvent = remapperFactory(getDate)
+
+var compileCalendarFactory = function (remapper){
+    return calendar => {
+        let events = calendar.map(remapper)
+        return {calendar: {events}}
+    }
 };
 
+var compileCalendar = compileCalendarFactory(remapEvent)
+
 module.exports = {
-    compileCalendar
+    compileCalendar,
+    compileCalendarFactory,
+
+    getDate,
+    remapEvent,
+    remapperFactory
 }
