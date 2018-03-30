5b03ad7233d008e5dad973e39c6cd6d8bfd00caa
Acksell <axelen@kth.se>
2018-03-30 20:59:25 +0200

    Integrated calendar with moment

--- frontend/src/js/data_compilers/calendarCompiler.js
+++ frontend/src/js/data_compilers/calendarCompiler.js
@@ -1,14 +1,21 @@
+const moment = require('moment')
+const locale = require('moment/locale/sv')
+moment.locale('sv')
+
 // Module containing functions which compile the calendar data fetched.
 
 var getDate = (event) => {
     let date
-    if (event.start.dateTime)
+    if (event.start.dateTime){
         date = event.start.dateTime
-    else if (event.start.date)
+        return moment(date).format('dddd D MMMM HH:mm')
+    }
+    else if (event.start.date){
         date =  event.start.date
+        return moment(date).format('dddd D MMMM')
+    }
     else 
-        date =  0
-    return new Date(date).toLocaleString()
+        return 0
 }
 
 var remapperFactory = function (dateGetter){
